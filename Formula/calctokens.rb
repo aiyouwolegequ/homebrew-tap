cdccf986e2305dcf4dd68db4b4fa775d7ef2b551
Darchrow <just4signinid@gmail.com>
2026-05-05 07:55:31 +0800

    Use pre-built binaries for both macOS arm64 and Linux

--- Formula/calctokens.rb
+++ Formula/calctokens.rb
@@ -3,16 +3,20 @@ class Calctokens < Formula
   homepage "https://github.com/aiyouwolegequ/CalcTokens"
   version "0.3.2"
 
-  url "https://github.com/aiyouwolegequ/CalcTokens/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "ad69e34479cf16869884e31d3c3f8ceb4deeecea97f909e671d1fd29deec3c15"
+  on_macos do
+    on_arm do
+      url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.3.2/calctokens-macos-arm64"
+      sha256 "57ed00c7e52db726004c1c77fb92c9423778d992cbac1d89c1abf335a29f4ece"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.3.2/calctokens"
+    sha256 "89bf5f1b217ac4d1661b9ce5757a48517a8281cfd1044b7b3d667b311264ae83"
+  end
 
   def install
-    # Allow Homebrew-installed binaries in PATH
-    ENV.delete("HOMEBREW_FORBID_PACKAGES_FROM_PATHS")
-    ENV.append_path "PATH", "#{HOMEBREW_PREFIX}/bin"
-
-    system "/opt/homebrew/bin/cargo", "build", "--release"
-    bin.install "target/release/calctokens"
+    bin.install "calctokens"
   end
 
   test do
