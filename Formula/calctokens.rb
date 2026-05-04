f25010be3c3dc515f964b7196aae961f1b4ef81f
Darchrow <just4signinid@gmail.com>
2026-05-05 02:16:35 +0800

    Add macOS source URL with clear error for arm64
    macOS arm64 must use cargo install. Provide a clear error message.

--- Formula/calctokens.rb
+++ Formula/calctokens.rb
@@ -8,18 +8,17 @@ class Calctokens < Formula
     sha256 "89bf5f1b217ac4d1661b9ce5757a48517a8281cfd1044b7b3d667b311264ae83"
   end
 
+  on_macos do
+    url "https://github.com/aiyouwolegequ/CalcTokens/archive/refs/tags/v0.3.2.tar.gz"
+    sha256 "ad69e34479cf16869884e31d3c3f8ceb4deeecea97f909e671d1fd29deec3c15"
+  end
+
   def install
     on_linux do
       bin.install "calctokens"
     end
-  end
-
-  def caveats
     on_macos do
-      <<~EOS
-        macOS/arm64: build from source with:
-          cargo install --git https://github.com/aiyouwolegequ/CalcTokens --tag v0.3.2
-      EOS
+      raise "macOS/arm64: use 'cargo install --git https://github.com/aiyouwolegequ/CalcTokens --tag v0.3.2' instead"
     end
   end
 
