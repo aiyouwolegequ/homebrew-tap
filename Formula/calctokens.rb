32a369261e9a817889f3cfe4998410c1617a300a
Darchrow <just4signinid@gmail.com>
2026-05-05 02:15:53 +0800

    Simplify formula: Linux pre-built binary, macOS via cargo install
    Building from source in Homebrew fails due to HOMEBREW_FORBID_PACKAGES_FROM_PATHS
    blocking Homebrew-installed cargo. Use cargo install for macOS users.

--- Formula/calctokens.rb
+++ Formula/calctokens.rb
@@ -3,12 +3,24 @@ class Calctokens < Formula
   homepage "https://github.com/aiyouwolegequ/CalcTokens"
   version "0.3.2"
 
-  url "https://github.com/aiyouwolegequ/CalcTokens/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "ad69e34479cf16869884e31d3c3f8ceb4deeecea97f909e671d1fd29deec3c15"
+  on_linux do
+    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.3.2/calctokens"
+    sha256 "89bf5f1b217ac4d1661b9ce5757a48517a8281cfd1044b7b3d667b311264ae83"
+  end
 
   def install
-    system "/opt/homebrew/bin/cargo", "build", "--release", "--locked"
-    bin.install "target/release/calctokens"
+    on_linux do
+      bin.install "calctokens"
+    end
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        macOS/arm64: build from source with:
+          cargo install --git https://github.com/aiyouwolegequ/CalcTokens --tag v0.3.2
+      EOS
+    end
   end
 
   test do
