e482e6f0fb421af574173efc3555e5b303a034ab
Darchrow <just4signinid@gmail.com>
2026-05-05 02:17:44 +0800

    Build from source with full cargo path

--- Formula/calctokens.rb
+++ Formula/calctokens.rb
@@ -3,28 +3,12 @@ class Calctokens < Formula
   homepage "https://github.com/aiyouwolegequ/CalcTokens"
   version "0.3.2"
 
-  on_linux do
-    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.3.2/calctokens"
-    sha256 "89bf5f1b217ac4d1661b9ce5757a48517a8281cfd1044b7b3d667b311264ae83"
-  end
-
-  on_macos do
-    url "https://github.com/aiyouwolegequ/CalcTokens/archive/refs/tags/v0.3.2.tar.gz"
-    sha256 "ad69e34479cf16869884e31d3c3f8ceb4deeecea97f909e671d1fd29deec3c15"
-    skip_install "macOS must build from source - use cargo install"
-  end
+  url "https://github.com/aiyouwolegequ/CalcTokens/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "ad69e34479cf16869884e31d3c3f8ceb4deeecea97f909e671d1fd29deec3c15"
 
   def install
-    on_linux do
-      bin.install "calctokens"
-    end
-  end
-
-  def caveats
-    <<~EOS
-      macOS/arm64: build from source with:
-        cargo install --git https://github.com/aiyouwolegequ/CalcTokens --tag v0.3.2
-    EOS
+    system "/opt/homebrew/bin/cargo", "build", "--release"
+    bin.install "target/release/calctokens"
   end
 
   test do
