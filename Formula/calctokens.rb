824bfea02b0b81a309809758c1d0e990f2471c3a
Darchrow <just4signinid@gmail.com>
2026-05-05 08:00:50 +0800

    Minimal formula with URL block syntax

--- Formula/calctokens.rb
+++ Formula/calctokens.rb
@@ -3,11 +3,11 @@ class Calctokens < Formula
   homepage "https://github.com/aiyouwolegequ/CalcTokens"
   version "0.3.2"
 
-  url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.3.2/calctokens-macos-arm64"
-  sha256 "57ed00c7e52db726004c1c77fb92c9423778d992cbac1d89c1abf335a29f4ece"
+  url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.3.2/calctokens-macos-arm64",
+    sha256: "57ed00c7e52db726004c1c77fb92c9423778d992cbac1d89c1abf335a29f4ece"
 
   def install
-    prefix.bin.install "calctokens-macos-arm64" => "calctokens"
+    bin.install "calctokens-macos-arm64"
   end
 
   test do
