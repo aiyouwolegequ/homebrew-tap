0d34e10ba28b0488dea562f351261de722aa098d
Darchrow <just4signinid@gmail.com>
2026-05-06 12:23:05 +0800

    chore: bump to v0.4.3 with correct Linux x86_64 binary

--- Formula/calctokens.rb
+++ Formula/calctokens.rb
@@ -1,14 +1,14 @@
 class Calctokens < Formula
   desc "Token usage report from Tokscale with K/M/B units & RMB conversion"
   homepage "https://github.com/aiyouwolegequ/CalcTokens"
-  version "0.4.2"
+  version "0.4.3"
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.4.2/calctokens-macos-arm64",
       sha256: "784166574f7d66b7e8c3bd32e7c951e4c9ccc7066638f4606c7fa25dc22bda91"
   else
-    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.4.2/calctokens",
-      sha256: "86423a888a526e354263964a6520cba9850802572384be9f9b8377f10da8e915"
+    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.4.3/calctokens",
+      sha256: "79bec80d4f8c74fdb351c5b3fe8ef21e63adb8f6f85736063097e7194187b5d8"
   end
 
   def install
