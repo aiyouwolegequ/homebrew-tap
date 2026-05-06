38abe4bd6d3a503ce928df5796ca4bf0b1d3d881
Darchrow <just4signinid@gmail.com>
2026-05-06 13:18:42 +0800

    chore: bump to v0.4.5 with correct dual-platform binaries

--- Formula/calctokens.rb
+++ Formula/calctokens.rb
@@ -1,13 +1,13 @@
 class Calctokens < Formula
   desc "Token usage report from Tokscale with K/M/B units & RMB conversion"
   homepage "https://github.com/aiyouwolegequ/CalcTokens"
-  version "0.4.3"
+  version "0.4.5"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.4.2/calctokens-macos-arm64",
-      sha256: "784166574f7d66b7e8c3bd32e7c951e4c9ccc7066638f4606c7fa25dc22bda91"
+    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.4.5/calctokens-macos-arm64",
+      sha256: "18a14ee366290fed3338ecb1993475eeb81905a66713781cffa1e373e193b88f"
   else
-    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.4.3/calctokens",
+    url "https://github.com/aiyouwolegequ/CalcTokens/releases/download/v0.4.5/calctokens",
       sha256: "79bec80d4f8c74fdb351c5b3fe8ef21e63adb8f6f85736063097e7194187b5d8"
   end
 
